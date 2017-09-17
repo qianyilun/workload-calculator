bbe9a99b4e9993730157f624f16b5a20e9901213
Yilun Qian <qianyilun@gmail.com>
2017-09-16 18:59:09 -0700

    implement DSM package by using inheritance

--- persistence-with-jdbc/src/com/allen/dsm/DSM.java
+++ persistence-with-jdbc/src/com/allen/dsm/DSM.java
@@ -1,5 +1,22 @@
 package com.allen.dsm;
 
-public class DSM {
+import com.allen.template.Template;
 
+public class DSM extends Template implements Comparable<DSM>{
+
+	@Override
+	public int compareTo(DSM dsm) {
+		// TODO Auto-generated method stub
+		double thisScore = super.getAmount() * 0.80 + (super.getTotal()-super.getAmount())/super.getAmount() * 0.20 + 10;
+		double dsmScore = dsm.getAmount() * 0.80 + (dsm.getTotal()-dsm.getAmount())/dsm.getAmount() * 0.20 + 10;
+
+		if (dsmScore > thisScore) {
+			return -1;
+		}
+		if (dsmScore < thisScore) {
+			return 1;
+		}
+		return 0;
+	}
+	
 }
