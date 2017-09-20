b20c6e5640635a6c2f106cd3c52f87e5fde805de
Yilun Qian <qianyilun@gmail.com>
2017-09-20 16:56:48 -0700

    refact the nw

---
+++ persistence-with-jdbc/src/com/allen/nw/NW.java
@@ -0,0 +1,32 @@
+package com.allen.nw;
+
+import com.allen.template.Template;
+
+/**
+ * Class holding information on a NW.
+ * 
+ * @author Allen Qian
+ */
+
+public class NW extends Template implements Comparable<NW>{
+
+	@Override
+	public int compareTo(NW nw) {
+		// TODO Auto-generated method stub
+		
+		if (super.getNw()==(double)0 || nw.getNw()==(double)0) {
+			return 0;
+		}
+		double thisScore = super.getNw() * 0.80 + (super.getTotal()-super.getNw())/super.getNw() * 0.20 + 10;
+		double nwScore = nw.getNw() * 0.80 + (nw.getTotal()-nw.getNw())/nw.getNw() * 0.20 + 10;
+
+		if (nwScore > thisScore) {
+			return -1;
+		}
+		if (nwScore < thisScore) {
+			return 1;
+		}
+		return 0;
+	}
+	
+}
---
+++ persistence-with-jdbc/src/com/allen/nw/NWDAO.java
@@ -0,0 +1,60 @@
+package com.allen.nw;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import com.allen.template.TemplateDAO;
+
+/**
+ * Data access object encapsulating all JDBC operations for NW.
+ * 
+ * @author Allen Qian
+ */
+public class NWDAO extends TemplateDAO{
+	private static final String COMPONENT = "NW";
+	public NWDAO(DataSource newDataSource) throws SQLException {
+		super(newDataSource);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+     * Get all entries from the table.
+     */
+    public List<NW> selectAllEntries() throws SQLException {
+        Connection connection = super.getDataSource().getConnection();
+        try {
+            PreparedStatement pstmt = connection
+                    .prepareStatement("SELECT ID, NAME," + COMPONENT + ",TOTAL "
+                    				+ "FROM ROOT");
+            ResultSet rs = pstmt.executeQuery();
+            ArrayList<NW> list = new ArrayList<NW>();
+            while (rs.next()) {
+                NW nw = new NW();
+                nw.setId(new Integer(rs.getInt(1)));
+                
+//                NW.setSum(super.getSum(NW.getId()));
+                
+                
+                nw.setName(rs.getString(2));
+                nw.setNw(rs.getInt(3));
+                nw.setTotal(rs.getInt(4));
+                list.add(nw);
+            }
+            Collections.sort(list);;
+            return list;
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+	
+}
