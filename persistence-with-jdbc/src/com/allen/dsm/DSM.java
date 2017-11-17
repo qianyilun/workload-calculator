0c5156de2c601ea25c2727be1281e50a5e45eb35
Yilun Qian <qianyilun@gmail.com>
2017-11-16 16:45:15 -0800

    Merge branch 'version_1.2.2'

--- persistence-with-jdbc/src/com/allen/dsm/DSM.java
+++ persistence-with-jdbc/src/com/allen/dsm/DSM.java
@@ -30,36 +30,3 @@ public class DSM extends Template implements Comparable<DSM>{
 		return 0;
 	}
 }
-=======
-package com.allen.dsm;
-
-import com.allen.QueueDays;
-import com.allen.template.Template;
-
-/**
- * Class holding information on a DSM.
- * 
- * @author Allen Qian
- */
-
-public class DSM extends Template implements Comparable<DSM>{
-	public double getPoint() {
-		return super.getDsm() * 0.80 + (super.getSum()-super.getDsm())/super.getDsm() * 0.20 + 10;
-		
-	}
-	
-	@Override
-	public int compareTo(DSM dsm) {
-		// TODO Auto-generated method stub
-		double thisScore = super.getSum() / QueueDays.hash.get(super.getName());
-		double dsmScore = dsm.getSum() / QueueDays.hash.get(dsm.getName());
-
-		if (dsmScore > thisScore) {
-			return -1;
-		}
-		if (dsmScore < thisScore) {
-			return 1;
-		}
-		return 0;
-	}
-}
--- persistence-with-jdbc/src/com/allen/sa/SA.java
+++ persistence-with-jdbc/src/com/allen/sa/SA.java
@@ -30,36 +30,3 @@ public class SA extends Template implements Comparable<SA>{
 		return 0;
 	}
 }
-=======
-package com.allen.sa;
-
-import com.allen.QueueDays;
-import com.allen.template.Template;
-
-/**
- * Class holding information on a SA.
- * 
- * @author Allen Qian
- */
-
-public class SA extends Template implements Comparable<SA>{
-	public double getPoint() {
-		return super.getSa() * 0.80 + (super.getSum()-super.getSa())/super.getSa() * 0.20 + 10;
-		
-	}
-	
-	@Override
-	public int compareTo(SA sa) {
-		// TODO Auto-generated method stub
-		double thisScore = super.getSum() / QueueDays.hash.get(super.getName());
-		double saScore = sa.getSum() / QueueDays.hash.get(sa.getName());
-		
-		if (saScore > thisScore) {
-			return -1;
-		}
-		if (saScore < thisScore) {
-			return 1;
-		}
-		return 0;
-	}
-}
